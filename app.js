4b92a39d95e137fe410d07188c12ecac8aaeec4e
yoursTrulyChandan <chandan.xt@outlook.com>
2023-05-03 12:24:26 +0530

    Tutorial till number  7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const names = require("./4-names");
 const sayHi = require("./5-utils");
 const data = require("./6-alternative-flavour");
-require("./7-mind-grenade");
+require("./7-mind-grenade"); 
